b60772bc625ed5a236166587a5962bb33c2a015a
Gergely Nagy <algernon@balabit.hu>
2011-11-16 16:16:03 +0100

    web: Separate program & host counters.

--- src/viharnaplo/web.clj
+++ src/viharnaplo/web.clj
@@ -8,14 +8,15 @@
             [clojure.data.json :as json])
   (:gen-class))
 
-(defn- redis-to-json []
+(defn- redis-to-json [prefix]
   (let [dbkeys (redis/with-connection *redis*
-                 (redis/keys "*"))
+                 (redis/keys (str prefix ".*")))
         dbvals (map #(redis/with-connection *redis* (redis/get %)) dbkeys)]
     (json/json-str (zipmap dbkeys dbvals))))
 
 (compojure/defroutes main-routes
-  (compojure/GET "/data.json" [] (redis-to-json))
+  (compojure/GET "/host.json" [] (redis-to-json "host"))
+  (compojure/GET "/program.json" [] (redis-to-json "program"))
   (route/files "/" {:root "resources/public"}))
 
 (def app
