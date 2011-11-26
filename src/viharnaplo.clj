525ffe841213585a80da9c4888cda921b42c1860
Gergely Nagy <algernon@madhouse-project.org>
2011-11-26 15:35:42 +0100

    viharnaplo: Support running for N seconds.
    Instead of hardcoding how long the cluster shall be ran, allow the
    user to specify it via the command line.

--- src/viharnaplo.clj
+++ src/viharnaplo.clj
@@ -3,9 +3,17 @@
   (:use [viharnaplo.config])
   (:require [viharnaplo.storm :as viharnaplo]))
 
-;; The main entry point, which just runs the LocalCluster forever.
+;; The main entry point, which just runs the LocalCluster forever,
+;; unless an argument is given. In that case, it runs the cluster for
+;; the specified amount of seconds.
 ;;
 ;; Usage:
 ;; <pre><code>$ lein run -m viharnaplo</code></pre>
-(defn -main []
-  (viharnaplo/run))
+;; Or
+;; <pre><code>$ lein run -m viharnaplo 10</code></pre>
+(defn -main [& args]
+  (let [cluster (viharnaplo/run)]
+    (if (not (empty? args))
+      (do
+        (Thread/sleep (* 1000 (Integer/parseInt (first args))))
+        (.shutdown cluster)))))
